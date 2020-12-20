adabea1af91823a3c87de0155520eedba2ae8c82
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-12-20 23:55:31 +0530

    final stopElement function

--- script.js
+++ script.js
@@ -13,13 +13,14 @@ dragElement(document.getElementById('plant12'));
 dragElement(document.getElementById('plant13'));
 dragElement(document.getElementById('plant14'));
 
-function dragElement(terrariumElement){
+function dragElement(terrariumElement) {
     //set 4 positions
-    let pos1=0,
-        pos2=0,
-        pos3=0,
-        pos4=0;
+    let pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
     terrariumElement.onpointerdown = pointerDrag;
+
     function pointerDrag(e) {
         e.preventDefault();
         console.log(e);
@@ -39,4 +40,10 @@ function dragElement(terrariumElement){
         terrariumElement.style.top = terrariumElement.offsetTop - pos2 + "px";
         terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + "px";
     }
+
+    function stopElementDrag() {
+        document.onpointerup = null;
+        document.onpointermove = null;
+    }
+
 }
