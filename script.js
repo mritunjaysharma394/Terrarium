5c30c80fc91c0dbb53945e3bdc5cd4c3dc547735
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-12-20 23:31:02 +0530

    dragElement function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//
+dragElement(document.getElementById('plant1'));
+dragElement(document.getElementById('plant2'));
+dragElement(document.getElementById('plant3'));
+dragElement(document.getElementById('plant4'));
+dragElement(document.getElementById('plant5'));
+dragElement(document.getElementById('plant6'));
+dragElement(document.getElementById('plant7'));
+dragElement(document.getElementById('plant8'));
+dragElement(document.getElementById('plant9'));
+dragElement(document.getElementById('plant10'));
+dragElement(document.getElementById('plant11'));
+dragElement(document.getElementById('plant12'));
+dragElement(document.getElementById('plant13'));
+dragElement(document.getElementById('plant14'));
+
+function dragElement(terrariumElement){
+    //set 4 positions
+    let pos1=0,
+        pos2=0,
+        pos3=0,
+        pos4=0;
+    terrariumElement.onpointerdown = pointerDrag;
+}
